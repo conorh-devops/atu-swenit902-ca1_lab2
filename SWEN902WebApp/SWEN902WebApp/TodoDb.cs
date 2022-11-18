8328420c80201154df6c1c4960d836bc93563cb2
Conor Horan <conorhoran@cbe.ie>
2022-11-18 23:49:28 +0000

    feat: First commit

---
+++ SWEN902WebApp/SWEN902WebApp/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿namespace SWEN902WebApp
+{
+    using Microsoft.EntityFrameworkCore;
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
