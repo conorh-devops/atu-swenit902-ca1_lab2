28e2891c842adb548b8c9af1305adf589f99e47d
Conor Horan <conorhoran@cbe.ie>
2022-11-19 00:43:59 +0000

    feat: Added DTO and secret field

--- SWEN902WebApp/SWEN902WebApp/Todo.cs
+++ SWEN902WebApp/SWEN902WebApp/Todo.cs
@@ -1,9 +1,10 @@
 ﻿namespace SWEN902WebApp
 {
-    class Todo
+    public class Todo
     {
         public int Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
     }
 }
